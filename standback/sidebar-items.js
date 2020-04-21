d66c560e029286677ea308bd17186fe11ef6edc4
jhpratt <the.z.cuber@gmail.com>
2020-04-21 01:42:26 +0000

    Deploying to gh-pages from  @ 3ceaacf74ce3b65471da61ffa74ba00b14248983 🚀

--- standback/sidebar-items.js
+++ standback/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["array",""],["convert",""],["iter",""],["marker",""],["mem",""],["pin",""],["prelude",""],["ptr",""],["task",""]]});
+initSidebarItems({"mod":[["array",""],["convert",""],["iter",""],["marker",""],["mem",""],["num",""],["pin",""],["prelude",""],["ptr",""],["task",""]]});
